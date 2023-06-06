6d9c62c54922eb1848caa62cc4069eca63013d1a
unknown <jonathanperozi16@gmail.com>
2023-06-06 18:19:34 -0300

    versao sem api

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -71,7 +71,7 @@ const Auth = () => {
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <img src="/images/logo.png" className="h-12" alt="Logo" />
+          <img src="/images/logo.png" className="h-48" alt="Logo" />
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
